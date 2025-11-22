431198c5b09b782a70f94bf499982e6dd4ac4985
avilaivan <ivanavilanazareno@gmail.com>
2025-11-22 13:28:40 -0300

    agregamos criatura, criatura salvaje, test de creacion y entrenamiento. Condiciones y excepciones controladas para maestria insuficiente en maestro para entrenar

---
+++ src/ar/edu/unlam/pb2/sistema/elemental/MaestriaInsuficienteException.java
@@ -0,0 +1,8 @@
+package ar.edu.unlam.pb2.sistema.elemental;
+
+public class MaestriaInsuficienteException extends Exception {
+    
+	public MaestriaInsuficienteException(String mensaje) {
+        super(mensaje);
+    }
+}
--- test/ar/edu/unlam/pb2/sistema/elemental/SistemaElementalTest.java
+++ test/ar/edu/unlam/pb2/sistema/elemental/SistemaElementalTest.java
@@ -5,11 +5,33 @@ import org.junit.Test;
 
 public class SistemaElementalTest {
 
-	@Test
-	public void queSePuedaCrearUnaCriaturaSalvaje() {
-		
-		CriaturaSalvaje criatura = new CriaturaSalvaje("Pyros", 100, Tipo.FUEGO);
-		assertEquals("Pyros", criatura.getNombre());
-		assertEquals(Integer.valueOf(100), criatura.getEnergia());
-	}
+    @Test
+    public void queSePuedaCrearUnaCriaturaSalvaje() {
+        CriaturaSalvaje criatura = new CriaturaSalvaje("Pyros", 100, Tipo.FUEGO);
+        assertEquals("Pyros", criatura.getNombre());
+        assertEquals(Integer.valueOf(100), criatura.getEnergia());
+    }
+
+
+    @Test
+    public void queUnMaestroPuedaAgregarUnaCriaturaASuColeccion() {
+        Maestro maestro = new Maestro("Aang", 50, Tipo.AIRE);
+        CriaturaSalvaje criatura = new CriaturaSalvaje("Appa", 100, Tipo.AIRE);
+
+        maestro.agregarCriatura(criatura);
+
+        Criatura encontrada = maestro.buscarCriatura("Appa");
+        assertNotNull(encontrada);
+        assertEquals(criatura, encontrada);
+    }
+
+    @Test(expected = MaestriaInsuficienteException.class)
+    public void queUnMaestroNovatoNoPuedaEntrenarYLanceExcepcion() throws Exception {
+        Maestro novato = new Maestro("Sokka", 1, Tipo.AGUA);
+        CriaturaSalvaje criatura = new CriaturaSalvaje("Pez", 10, Tipo.AGUA);
+        
+        novato.agregarCriatura(criatura);
+
+        novato.entrenar("Pez"); 
+    }
 }
