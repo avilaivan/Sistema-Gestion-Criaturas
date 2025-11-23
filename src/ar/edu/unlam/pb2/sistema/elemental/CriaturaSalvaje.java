010118f5c64b5f280cb99f9c4ec808af7b957bf1
valenhimitian <valentinhimitian@gmail.com>
2025-11-22 23:10:29 -0300

    Cambio de numeros magicos/sueltos a constantes para la clase CriaturaSalvaje
    Replaced magic numbers with constants for training power and energy limit.

--- src/ar/edu/unlam/pb2/sistema/elemental/CriaturaSalvaje.java
+++ src/ar/edu/unlam/pb2/sistema/elemental/CriaturaSalvaje.java
@@ -2,15 +2,21 @@ package ar.edu.unlam.pb2.sistema.elemental;
 
 public class CriaturaSalvaje extends Criatura {
 
+    // Numeros sueltos o magicos a constantes
+    private static final Integer POTENCIA_ENTRENAMIENTO = 50;
+    private static final Integer ENERGIA_DESCONTROL = 200;
+
     public CriaturaSalvaje(String nombre, Integer energia, Tipo tipo) {
         super(nombre, energia, tipo);
     }
 
     @Override
     public void entrenar() throws EnergiaDesbordadaException {
-        this.energia = this.energia + 50; 
         
-        if (this.energia > 200) {
+        this.energia = this.energia + POTENCIA_ENTRENAMIENTO; 
+        
+        
+        if (this.energia > ENERGIA_DESCONTROL) {
             throw new EnergiaDesbordadaException("¡La criatura " + nombre + " se ha descontrolado!");
         }
     }
@@ -19,8 +25,4 @@ public class CriaturaSalvaje extends Criatura {
     public void disminuirEnergia(Integer cantidad) {
         this.energia = this.energia - cantidad;
     }
-    
-    
-    
-    
 }
